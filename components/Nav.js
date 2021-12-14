cd5d84017185c65b09d13d200df5dd1d3db706e1
Dong <dongpd224@gmail.com>
2021-12-14 17:42:58 +0700

    add bootstrap

---
+++ components/Nav.js
@@ -0,0 +1,11 @@
+
+function Nav(){
+    return(
+        <div className="container-fluid primary-background-color">
+            <div className="container">
+                <h1>Hello</h1>
+            </div>
+        </div>
+    )
+}
+export default Nav
