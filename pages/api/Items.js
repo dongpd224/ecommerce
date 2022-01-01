45fb1906ae3b56d719270e081742fcf8b444ae9b
dongpd224 <dongpd224@gmail.com>
2022-01-01 11:21:54 +0700

    change name

--- pages/api/Items.js
+++ pages/api/Items.js
@@ -1,32 +1,9 @@
-// import data from './items_data.json'
-const fs = require('fs')
-const pathToFile = './items_data.json'
-let dbraw = fs.readFileSync(pathToFile);
-const data = JSON.parse(dbraw);
+import data from './items_data.json'
+
 
 export default async function (req, res) {
     if (req.method === "GET") {
         return res.send(data);
-    }
-    if (req.method === "POST") {
-        const item = req.body;
-        if (!item.name || !item.discount_price || !item.img_link) {
-            return res.status(422).send("Data are missing!");
-        }
-        else {
-            try {
-                item.id = Date.now().toString()
-                data.push(item)
-                fs.writeFileSync(pathToFile, JSON.stringify(data))
-                { 
-                    return res.status(200).send(JSON.parse(data)); 
-                }
-            }
-
-            catch {
-                return res.status(422).send("Cannot update the data");
-            }
-        }
     }
 }
 
