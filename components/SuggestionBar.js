8f6c0226f366ec9be0b38379bb27a2b723162d4f
dongpd224 <dongpd224@gmail.com>
2022-01-04 21:48:02 +0700

    suggestion bar

--- components/SuggestionBar.js
+++ components/SuggestionBar.js
@@ -9,59 +9,62 @@ function SuggestionBar() {
             <div className={`${styles.suggestion_header}`}>
                 <span>Gợi Ý Hôm Nay</span>
             </div>
-            <div className={`row ${styles.suggestion_items}`}>
-                <div className='col-6'>
-                    <div className='row'>
-                        <div className={`col-lg-3 col-md-6`}>
-                            <div className={`${styles.suggestion_item}`}>
-                                <Image src={Icon.DanhChoBan} alt='Danh cho ban' width={48} height={48} />
-                                <span>Dành cho bạn</span>
+
+            <div className=' d-none d-lg-block'>
+                <div className={`row ${styles.suggestion_items}`}>
+                    <div className='col-6'>
+                        <div className='row'>
+                            <div className={`col-lg-3 col-md-6`}>
+                                <div className={`${styles.suggestion_item}`}>
+                                    <Image src={Icon.DanhChoBan} alt='Danh cho ban' width={48} height={48} />
+                                    <span>Dành cho bạn</span>
+                                </div>
                             </div>
-                        </div>
-                        <div className={`col-lg-3 col-md-6`}>
-                            <div className={`${styles.suggestion_item}`}>
-                                <Image src={Icon.ChiTu11k} alt='Icon' width={48} height={48} />
-                                <span>Chỉ từ 11k</span>
+                            <div className={`col-lg-3 col-md-6`}>
+                                <div className={`${styles.suggestion_item}`}>
+                                    <Image src={Icon.ChiTu11k} alt='Icon' width={48} height={48} />
+                                    <span>Chỉ từ 11k</span>
+                                </div>
                             </div>
-                        </div>
-                        <div className={`col-lg-3 col-md-6`}>
-                            <div className={`${styles.suggestion_item}`}>
-                                <Image src={Icon.DealSieuHot} alt='Icon' width={48} height={48} />
-                                <span>Deal siêu hot</span>
+                            <div className={`col-lg-3 col-md-6`}>
+                                <div className={`${styles.suggestion_item}`}>
+                                    <Image src={Icon.DealSieuHot} alt='Icon' width={48} height={48} />
+                                    <span>Deal siêu hot</span>
+                                </div>
                             </div>
-                        </div>
-                        <div className={`col-lg-3 col-md-6`}>
-                            <div className={`${styles.suggestion_item}`}>
-                                <Image src={Icon.ReVoDoi} alt='Icon' width={48} height={48} />
-                                <span>Rẻ vô đối</span>
+                            <div className={`col-lg-3 col-md-6`}>
+                                <div className={`${styles.suggestion_item}`}>
+                                    <Image src={Icon.ReVoDoi} alt='Icon' width={48} height={48} />
+                                    <span>Rẻ vô đối</span>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className='col-6'>
-                    <div className='row'>
-                        <div className={`col-lg-3 col-md-6`}>
-                            <div className={`${styles.suggestion_item}`}>
-                                <Image src={Icon.DiChoSieuSale} alt='Icon' width={48} height={48} />
-                                <span>Đi Chợ Siêu Sale</span>
+                    <div className='col-6'>
+                        <div className='row'>
+                            <div className={`col-lg-3 col-md-6`}>
+                                <div className={`${styles.suggestion_item}`}>
+                                    <Image src={Icon.DiChoSieuSale} alt='Icon' width={48} height={48} />
+                                    <span>Đi Chợ Siêu Sale</span>
+                                </div>
                             </div>
-                        </div>
-                        <div className={`col-lg-3 col-md-6`}>
-                            <div className={`${styles.suggestion_item}`}>
-                                <Image src={Icon.HangMoi} alt='Icon' width={48} height={48} />
-                                <span>Hàng Mới</span>
+                            <div className={`col-lg-3 col-md-6`}>
+                                <div className={`${styles.suggestion_item}`}>
+                                    <Image src={Icon.HangMoi} alt='Icon' width={48} height={48} />
+                                    <span>Hàng Mới</span>
+                                </div>
                             </div>
-                        </div>
-                        <div className={`col-lg-3 col-md-6`}>
-                            <div className={`${styles.suggestion_item}`}>
-                                <Image src={Icon.XuHuongThoiTrang} alt='Icon' width={48} height={48} />
-                                <span>Xu Hướng Thời Trang</span>
+                            <div className={`col-lg-3 col-md-6`}>
+                                <div className={`${styles.suggestion_item}`}>
+                                    <Image src={Icon.XuHuongThoiTrang} alt='Icon' width={48} height={48} />
+                                    <span>Xu Hướng Thời Trang</span>
+                                </div>
                             </div>
-                        </div>
-                        <div className={`col-lg-3 col-md-6`}>
-                            <div className={`${styles.suggestion_item}`}>
-                                <Image src={Icon.FreeshipKhongGioiHan} alt='Icon' width={48} height={48} />
-                                <span>Không Giới Hạn</span>
+                            <div className={`col-lg-3 col-md-6`}>
+                                <div className={`${styles.suggestion_item}`}>
+                                    <Image src={Icon.FreeshipKhongGioiHan} alt='Icon' width={48} height={48} />
+                                    <span>Không Giới Hạn</span>
+                                </div>
                             </div>
                         </div>
                     </div>
